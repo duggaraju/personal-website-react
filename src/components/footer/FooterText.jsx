a5f464e8a238e73a78a2809cde8e6f0833b9b954
Prakash Duggaraju <krishndu@microsoft.com>
2024-03-15 00:25:45 -0700

    Upgrade to vite from create-react-app Rename *.js to *.jsx

--- src/components/footer/FooterText.jsx
+++ src/components/footer/FooterText.jsx
@@ -26,7 +26,7 @@ export const FooterText = () => {
     <Link
       color='inherit'
       underline='none'
-      href='https://github.com/sponsors/JoHoop'
+      href='https://github.com/sponsors/duggaraju'
       target='_blank'
       rel='noopener noreferrer'
       className={classes.footerText}
